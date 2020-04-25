ed4d83efe8f9685f3aa1e9804c3b2c317ccc9094
kalash14 <maxim.kalashnyk@gmail.com>
2020-04-26 02:00:17 +0300

    configuring apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AppContainer } from './styles';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { client } from '.';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export const App = () => (
+  <ApolloProvider client={client}>
+    <AppContainer>
+      <h2>My first Apollo app</h2>
+    </AppContainer>
+  </ApolloProvider>
+);
