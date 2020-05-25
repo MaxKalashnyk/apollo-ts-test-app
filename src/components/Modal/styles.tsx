0b9ae60bb908693adbb9af3f33563a6e4b4d3c29
kalash14 <maxim.kalashnyk@gmail.com>
2020-05-26 01:41:18 +0300

    fix: deploy error

---
+++ src/components/Modal/styles.tsx
@@ -0,0 +1,10 @@
+export const defaultModalStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
